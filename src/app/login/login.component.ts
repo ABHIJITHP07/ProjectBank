4ad078db5df297f5c096dc470669e0dbd04e8e4e
Abhijith Pilanku <abhijithp07@gmail.com>
2023-02-07 20:27:32 +0530

    third

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -10,25 +12,42 @@ export class LoginComponent implements OnInit {
   data="Your perfect banking partner"
   input="Account Number"
 
-  // acno=''   //or acno=any (need to initialise)
-  // pwd=''
+  acno=''                             //or acno=any (need to initialise)
+  pwd=''
 
-  userDetails:any={
-    1000:{acno:1000,username:"arun",password:"abc123",balance:0},
-    1001:{acno:1001,username:"anu",password:"abc123",balance:0},
-    1002:{acno:1002,username:"amal",password:"abc123",balance:0},
-    1003:{acno:1003,username:"abhi",password:"abc123",balance:0},
-    
-  }
 
-  constructor() {}
+  constructor(private router:Router,private ds:DataService) {}
 
   ngOnInit(): void {
   }
 
-  // login(){
-  //   var acno=this.acno
-  //   var pwd=this.pwd
+  login(){
+    var acno=this.acno
+    var pwd=this.pwd
+    var userDetails=this.ds.userDetails
+    if(acno in userDetails){
+      if(pwd==userDetails[acno]["password"]){
+        alert('login successful')
+        this.router.navigateByUrl('dashboard')
+
+      }
+      else{
+        alert('incorrect password')
+      }
+
+    }
+    else{
+      alert('acno incorrect or not registered')
+    }
+    //alert('login clicked')
+
+  }
+
+  // login(a:any,b:any){
+  //   console.log(a.value);
+    
+  //   var acno=a.value
+  //   var pwd=b.value
   //   var userDetails1=this.userDetails
   //   if(acno in userDetails1){
   //     if(pwd==userDetails1[acno]["password"]){
@@ -46,29 +65,6 @@ export class LoginComponent implements OnInit {
   //   //alert('login clicked')
 
   // }
-
-  login(a:any,b:any){
-    console.log(a.value);
-    
-    var acno=a.value
-    var pwd=b.value
-    var userDetails1=this.userDetails
-    if(acno in userDetails1){
-      if(pwd==userDetails1[acno]["password"]){
-        alert('login successful')
-
-      }
-      else{
-        alert('incorrect password')
-      }
-
-    }
-    else{
-      alert('acno incorrect or not registered')
-    }
-    //alert('login clicked')
-
-  }
   // acnoChange(event:any){
   //   this.acno=event.target.value   //alaways use (this.) while calling, data will be stored here event.target.value
   //   //console.log(this.acno);

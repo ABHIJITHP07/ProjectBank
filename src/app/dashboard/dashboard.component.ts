0226a841128c7d496207945245f123abcf94bf01
Abhijith Pilanku <abhijithp07@gmail.com>
2023-02-13 17:49:10 +0530

    fifth

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,16 +6,53 @@ import { DataService } from '../services/data.service';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
 
-  user:any
-  constructor(private ds:DataService)   {
+  user: any
 
-   this.user= this.ds.currentUser
+  acno1: any
+  psw1: any
+  amnt1: any
+
+  acno2: any
+  psw2: any
+  amnt2: any
+
+  constructor(private ds: DataService) {
+
+    this.user = this.ds.currentUser
 
   }
 
   ngOnInit(): void {
+  }
+
+  deposit() {
+    var acno = this.acno1
+    var psw = this.psw1
+    var amnt = this.amnt1
+
+    const result = this.ds.deposit(acno, psw, amnt)
+    if (result) {
+      alert(`your account has been credited with amount ${amnt},The new balance is ${result}`)
+    }
+    else {
+      alert('acno or password is incorrect')
+    }
+
+
+  }
+
+  withdraw() {                         //since both are in different method we can use same var name
+    var acno = this.acno2
+    var psw = this.psw2
+    var amnt = this.amnt2
+
+    const result = this.ds.withdraw(acno, psw, amnt)
+
+    if (result) {
+      alert(`Amount ${amnt} has been debited from your account. New account balance is ${result}`)
+    }
   }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,53 +5,107 @@ import { Injectable } from '@angular/core';
 })
 export class DataService {
 
-  currentUser:any
+  currentUser: any
 
   constructor() { }
 
-  userDetails:any={
-    1000:{acno:1000,username:"arun",password:"abc123",balance:0},
-    1001:{acno:1001,username:"anu",password:"abc123",balance:0},
-    1002:{acno:1002,username:"amal",password:"abc123",balance:0},
-    1003:{acno:1003,username:"abhi",password:"abc123",balance:0},
-    
+  userDetails: any = {
+    1000: { acno: 1000, username: "arun", password: "abc123", balance: 0 },
+    1001: { acno: 1001, username: "anu", password: "abc123", balance: 0 },
+    1002: { acno: 1002, username: "amal", password: "abc123", balance: 0 },
+    1003: { acno: 1003, username: "abhi", password: "abc123", balance: 0 },
+
   }
 
-  register(uname:any,acno:any,psw:any){
-    if(acno in this.userDetails){
+  register(uname: any, acno: any, psw: any) {
+    if (acno in this.userDetails) {
       return false
     }
-    else{
-      this.userDetails[acno]={acno,username:uname,password:psw,balance:0}
+    else {
+      this.userDetails[acno] = { acno, username: uname, password: psw, balance: 0 }
       console.log(this.userDetails);
-      
+
       return true
 
     }
   }
 
-  login(acno:any,psw:any){
+  login(acno: any, psw: any) {
 
-    var userDetails=this.userDetails
-    if(acno in userDetails){
-      if(psw==userDetails[acno]["password"]){
-        this.currentUser=userDetails[acno]["username"]
+    var userDetails = this.userDetails
+    if (acno in userDetails) {
+      if (psw == userDetails[acno]["password"]) {
+        this.currentUser = userDetails[acno]["username"]
         console.log(this.currentUser);
-        
+
         return true
 
       }
-      else{
+      else {
         return false
       }
 
     }
-    else{
+    else {
       return false
     }
-    
 
   }
 
-  
+  deposit(acnum: any, password: any, amount: any) {
+
+    var userDetails = this.userDetails
+    //convert string amount into number
+    var amnt = parseInt(amount)
+
+    if (acnum in userDetails) {
+      if (password == userDetails[acnum]["password"]) {
+
+        //update balance
+        userDetails[acnum]["balance"] += amnt
+
+        //return current balance
+        return userDetails[acnum]["balance"]
+      }
+      else {
+        return false
+      }
+    }
+    else {
+      return false
+    }
+  }
+
+  withdraw(acnum: any, password: any, amount: any) {
+
+    var userDetails = this.userDetails
+
+    var amnt = parseInt(amount)
+
+    if (acnum in userDetails) {
+      if (password == userDetails[acnum]["password"]) {
+
+        if (userDetails[acnum]["balance"] > amnt) {
+
+          userDetails[acnum]["balance"] -= amnt
+
+          return userDetails[acnum]["balance"]
+        }
+        else {
+          alert('insufficient balance')
+          return false
+        }
+
+
+      }
+      else {
+        alert('incorrect password')
+        return false
+      }
+    }
+    else {
+      alert('incorrect acno')
+      return false
+    }
+  }
 }
